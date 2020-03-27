2ce055b345e7fdc6da2a781ed9376b47e84d79b2
Alec PC <alecadab@gmail.com>
2020-03-27 15:33:41 +0800

    Copied P2 address book into P3

---
+++ P3/AddressBook/src/SearchByName.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class SearchByName extends Option
+{
+    /**
+     * Constructor
+     */
+    protected SearchByName(AddressBook inAddressBook)
+    {
+        addressBook = inAddressBook;
+    }
+
+    /**
+     * Finds the email address(es) for the given name
+     * 
+     * @param s The name string to look up
+     * @return Result string
+     * @throws NoSuchElementException If no entry is found for the name
+     */
+    @Override public String doOption(String s) throws NoSuchElementException
+    {
+        return addressBook.findEmail(s);
+    }
+
+    @Override public boolean requiresText()
+    {
+        return true;
+    }
+}
